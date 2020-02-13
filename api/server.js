db1b564367fa88a018fe18644defd687a109bf8f
taterntots <matthew.a.weidner@gmail.com>
2020-02-13 15:40:50 -0500

    set up a basic server

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const server = express();
+
+//global middleware
+server.use(express.json()); //middleware needed to parse JSON
+server.use(helmet()); //middleware that adds a layer of security to the server
+server.use(cors()); //middleware that allows cross domain communication from the browser
+
+//endpoints
+server.get('/', (req, res) => {
+  res.status(200).json({ welcome: `to the danger zone!` });
+});
+
+//routes
+// server.use('/api/auth', authRouter);
+
+module.exports = server
